b12e449557540e99f03d97dfd96564ebc2829a49
Anton Bakavets <anton.bakavets@instructure.com>
2022-07-26 23:32:29 +0300

    Terraform

--- terraform/helm.tf
+++ terraform/helm.tf
@@ -1,13 +1,63 @@
+locals {
+  argocd = {
+    namespace = "argocd"
+  }
+  initial_bootstrap = {
+    repoURL               = "https://github.com/bakavetsbiz/demo-dsd.git"
+    targetRevision        = "main"
+    destination_namespace = "argocd"
+    source_path           = "k8s/infrastructure/applications/"
+  }
+}
+
 resource "helm_release" "argocd" {
   name = "argocd"
 
   repository       = "https://argoproj.github.io/argo-helm"
   chart            = "argo-cd"
   version          = "4.9.16"
-  namespace        = "argocd"
+  namespace        = local.argocd.namespace
   create_namespace = true
 
   depends_on = [
     module.eks
+  ]
+}
+
+resource "kubectl_manifest" "initial_bootstrap" {
+  yaml_body = <<YAML
+apiVersion: argoproj.io/v1alpha1
+kind: Application
+metadata:
+  name: initial-bootstrap
+  namespace: ${local.argocd.namespace}
+  finalizers:
+    - resources-finalizer.argocd.argoproj.io
+spec:
+  project: default
+  source:
+    path: ${local.initial_bootstrap.source_path}
+    repoURL: ${local.initial_bootstrap.repoURL}
+    targetRevision: ${local.initial_bootstrap.targetRevision}
+
+    helm:
+      parameters:
+      - name: targetRevision
+        value: ${local.initial_bootstrap.targetRevision}
+
+  destination:
+    namespace: ${local.argocd.namespace}
+    server: https://kubernetes.default.svc
+
+  syncPolicy:
+    automated:
+      prune: true
+      selfHeal: true
+    retry:
+      limit: -1
+YAML
+
+  depends_on = [
+    helm_release.argocd
   ]
 }
--- terraform/main.tf
+++ terraform/main.tf
@@ -5,6 +5,10 @@ terraform {
       source  = "hashicorp/aws"
       version = ">= 3.72"
     }
+    kubectl = {
+      source  = "gavinbunney/kubectl"
+      version = ">= 1.7.0"
+    }
   }
 }
 
@@ -100,4 +104,15 @@ provider "helm" {
       args = ["eks", "get-token", "--cluster-name", module.eks.cluster_id]
     }
   }
+}
+
+provider "kubectl" {
+  host                   = module.eks.cluster_endpoint
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  token                  = data.aws_eks_cluster_auth.cluster.token
+  load_config_file       = false
+}
+
+data "aws_eks_cluster_auth" "cluster" {
+  name = module.eks.cluster_id
 }
